4c8e38af6294fc9544c27d16f5b65337dbf0b1f1
Lunar-Dawn <lunarrequiem42@gmail.com>
2025-08-12 11:44:36 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if ! [[ -d "$1" ]]; then
+	echo "Export directory $1 does not exist. Aborting."
+	exit 1
+fi
+
+DIR="$1/TrueNonSenescence"
+echo $DIR
+
+if [[ -d $DIR ]]; then
+	rm -r $DIR
+fi
+
+mkdir $DIR
+cp -r ./About $DIR
+cp -r ./1.6 $DIR
