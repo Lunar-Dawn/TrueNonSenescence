4b292b6e1bc9d1fe217e8414d74c417a76bdbbe9
Lunar-Dawn <lunarrequiem42@gmail.com>
2025-11-14 02:41:36 +0100

    Move and make postfix the last few numerical patches

---
+++ Source/TrueNonSenescence/PatchNumericalFactors.cs
@@ -0,0 +1,74 @@
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace TrueNonSenescence
+{
+	[StaticConstructorOnStartup]
+	public static class PatchNumericalFactors
+	{
+		private static readonly StatDef ImmunityGainSpeed = DefDatabase<StatDef>.GetNamed("ImmunityGainSpeed");
+
+		static PatchNumericalFactors()
+		{
+			var harmony = new Harmony("LunarDawn.TrueNonSenescence");
+
+			harmony.Patch(
+				AccessTools.Method(typeof(StatPart_Age), "AgeMultiplier"),
+				postfix: new HarmonyMethod(typeof(PatchNumericalFactors), nameof(PatchImmunity))
+			);
+			harmony.Patch(
+				AccessTools.Method(typeof(Pawn_RelationsTracker), nameof(Pawn_RelationsTracker.LovinAgeFactor)),
+				postfix: new HarmonyMethod(typeof(PatchNumericalFactors), nameof(PatchRomance))
+			);
+
+			harmony.Patch(
+				AccessTools.Method(typeof(StatPart_FertilityByGenderAge), "AgeFactor"),
+				postfix: new HarmonyMethod(typeof(PatchNumericalFactors), nameof(PatchFertility))
+			);
+			// "Fertility scale with lifespan" replaces the entire StatPart for fertility age
+			// with its own custom percentage-based one, so we patch it if it exists.
+			var fertilityByGenderLifeSpanAgeFactor = AccessTools.Method(
+				AccessTools.TypeByName("FertilityScaleWithLifeSpan.StatPart_FertilityByGenderLifeSpan"),
+				"AgeFactor");
+			if (fertilityByGenderLifeSpanAgeFactor != null)
+			{
+				harmony.Patch(
+					fertilityByGenderLifeSpanAgeFactor,
+					postfix: new HarmonyMethod(typeof(PatchNumericalFactors), nameof(PatchFertility))
+				);
+			}
+		}
+
+		private static void PatchImmunity(ref float __result, StatPart_Age __instance, Pawn pawn)
+		{
+			// Check if we're calculating Immunity Gain Speed
+			if (__instance.parentStat != ImmunityGainSpeed)
+				return;
+
+			if (!Cache.PawnIsNonSenescent(pawn))
+				return;
+
+			__result = 1.0f;
+		}
+
+		private static void PatchRomance(ref float __result, Pawn otherPawn, Pawn ___pawn)
+		{
+			if (!Cache.PawnIsNonSenescent(___pawn) || !Cache.PawnIsNonSenescent(otherPawn))
+				return;
+
+			float num1 = Mathf.InverseLerp(16f, 18f, ___pawn.ageTracker.AgeBiologicalYearsFloat);
+			float num2 = Mathf.InverseLerp(16f, 18f, otherPawn.ageTracker.AgeBiologicalYearsFloat);
+			__result = num1 * num2;
+		}
+
+		private static void PatchFertility(ref float __result, Pawn pawn)
+		{
+			if (!Cache.PawnIsNonSenescent(pawn))
+				return;
+
+			__result = 1.0f;
+		}
+	}
+}
